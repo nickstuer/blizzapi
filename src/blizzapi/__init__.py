cd1775363bf1d5859c89c50d0194f9c68149c92f
Nick Stuer <nickstuer@gmail.com>
2025-01-01 20:26:21 -0500

    attempts to version pypi

--- src/blizzapi/__init__.py
+++ src/blizzapi/__init__.py
@@ -1,2 +1,10 @@
+import importlib.metadata
+DISTRIBUTION_NAME = "blizzapi"
+
+__version__ = importlib.metadata.version(DISTRIBUTION_NAME)
+
 from .clients.wow.classicEraClient import ClassicEraClient
 from .core.enums import Language, Region
+
+
+print(__version__)
